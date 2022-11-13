fe7566c6376b88b95e7dbbfc0f09040c354ec608
Cierra-Runis <2864283875@qq.com>
2022-11-13 20:45:02 +0800

    修改📖️ 1. 略

--- lib/common/devtools.dart
+++ lib/common/devtools.dart
@@ -4,7 +4,7 @@ import 'package:mercurius/index.dart';
 class DevTools {
   // 在这改设定
   static const showLog = true;
-  static const showDebugPaintSizeEnabled = true;
+  static const showDebugPaintSizeEnabled = false;
 
   // 调试用输出语句
   static void printLog(String message) {
