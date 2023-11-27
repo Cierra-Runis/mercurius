c2931780272986054b2c62dfa3e622379c044f30
Cierra_Runis <2864283875@qq.com>
2023-11-27 19:48:28 +0800

    📖Improve style of editor body

--- lib/widgets/editor/editor_body.dart
+++ lib/widgets/editor/editor_body.dart
@@ -21,6 +21,17 @@ class EditorBody extends StatelessWidget {
 
   static const unknownEmbedBuilder = DeprecatedEmbedBuilder();
 
+  void onLaunchUrl(url) {
+    try {
+      launchUrlString(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    } catch (e) {
+      App.printLog('launch $url failed: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10N.current;
@@ -29,53 +40,192 @@ class EditorBody extends StatelessWidget {
       focusNode: FocusNode(),
       scrollController: scrollController,
       configurations: QuillEditorConfigurations(
+        padding: const EdgeInsets.all(2.0),
+        autoFocus: autoFocus,
+        expands: false,
+        placeholder: l10n.writingSomethingHere,
+        keyboardAppearance: context.brightness,
         scrollable: true,
         enableInteractiveSelection: enableInteractiveSelection,
         enableSelectionToolbar: enableSelectionToolbar,
         showCursor: showCursor,
-        expands: false,
-        padding: const EdgeInsets.all(2.0),
-        autoFocus: autoFocus,
-        placeholder: l10n.writingSomethingHere,
         readOnly: readOnly,
-        onLaunchUrl: (url) {
-          try {
-            launchUrlString(
-              url,
-              mode: LaunchMode.externalApplication,
-            );
-          } catch (e) {
-            App.printLog('launch $url failed: $e');
-          }
-        },
+        onLaunchUrl: onLaunchUrl,
         scrollBottomInset: 10,
         embedBuilders: embedBuilders,
         unknownEmbedBuilder: unknownEmbedBuilder,
-        customStyles: DefaultStyles(
-          placeHolder: DefaultTextBlockStyle(
-            TextStyle(
-              fontFamily: 'Saira',
-              fontSize: 14,
-              height: 1.5,
-              color: Colors.grey.withOpacity(0.6),
-            ),
-            const VerticalSpacing(3, 3),
-            const VerticalSpacing(0, 0),
-            null,
-          ),
-          paragraph: DefaultTextBlockStyle(
-            TextStyle(
-              fontFamily: 'Saira',
-              fontSize: 14,
-              height: 1.5,
-              color: context.colorScheme.inverseSurface,
-            ),
-            const VerticalSpacing(3, 3),
-            const VerticalSpacing(0, 0),
-            null,
-          ),
-        ),
+        customStyles: customStyles(context),
       ),
     );
   }
+}
+
+DefaultStyles customStyles(BuildContext context) {
+  final baseStyle = TextStyle(
+    color: context.colorScheme.onBackground,
+    fontFamily: App.fontSaira,
+    fontFamilyFallback: const [App.fontCascadiaCodePL, App.fontMiSans],
+    height: 4 / 3,
+  );
+
+  const verticalSpacing = VerticalSpacing(4, 4);
+  const lineSpacing = VerticalSpacing(0, 0);
+
+  final h1 = DefaultTextBlockStyle(
+    baseStyle.copyWith(fontSize: 20),
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+  );
+
+  final h2 = DefaultTextBlockStyle(
+    baseStyle.copyWith(fontSize: 18),
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+  );
+
+  final h3 = DefaultTextBlockStyle(
+    baseStyle.copyWith(fontSize: 16),
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+  );
+
+  final paragraph = DefaultTextBlockStyle(
+    baseStyle,
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+  );
+
+  final bold = baseStyle.copyWith(
+    fontWeight: FontWeight.bold,
+  );
+  final subscript = baseStyle.copyWith(
+    fontFeatures: [const FontFeature.subscripts()],
+  );
+  final superscript = baseStyle.copyWith(
+    fontFeatures: [const FontFeature.superscripts()],
+  );
+  final italic = baseStyle.copyWith(
+    fontStyle: FontStyle.italic,
+  );
+  final small = baseStyle.copyWith(
+    fontSize: 12,
+  );
+  final underline = baseStyle.copyWith(
+    decoration: TextDecoration.underline,
+  );
+  final strikeThrough = baseStyle.copyWith(
+    decoration: TextDecoration.lineThrough,
+  );
+
+  /// FIXME: backgroundColor no effect <https://github.com/singerdmx/flutter-quill/issues/1409>
+  final inlineCode = InlineCodeStyle(
+    style: baseStyle,
+    backgroundColor: null,
+    radius: const Radius.circular(120),
+  );
+
+  final link = small.copyWith(
+    color: context.colorScheme.primary,
+    fontFamily: App.fontCascadiaCodePL,
+    decoration: TextDecoration.underline,
+  );
+
+  final placeholder = DefaultTextBlockStyle(
+    baseStyle.copyWith(
+      color: context.colorScheme.outline,
+    ),
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+  );
+
+  final lists = DefaultListBlockStyle(
+    small.copyWith(fontFamily: App.fontCascadiaCodePL),
+    verticalSpacing,
+    lineSpacing,
+    const BoxDecoration(),
+    null,
+  );
+
+  final quote = DefaultTextBlockStyle(
+    placeholder.style,
+    verticalSpacing,
+    lineSpacing,
+    BoxDecoration(
+      border: Border(
+        left: BorderSide(
+          color: context.colorScheme.primary,
+          width: 4,
+        ),
+      ),
+    ),
+  );
+
+  final code = DefaultTextBlockStyle(
+    small.copyWith(
+      color: context.colorScheme.secondary,
+      fontFamily: App.fontCascadiaCodePL,
+    ),
+    verticalSpacing,
+    lineSpacing,
+    BoxDecoration(
+      color: context.colorScheme.secondaryContainer,
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+
+  final indent = DefaultTextBlockStyle(
+    baseStyle,
+    verticalSpacing,
+    lineSpacing,
+    null,
+  );
+
+  final align = DefaultTextBlockStyle(
+    baseStyle,
+    verticalSpacing,
+    lineSpacing,
+    null,
+  );
+
+  final leading = DefaultTextBlockStyle(
+    baseStyle,
+    verticalSpacing,
+    lineSpacing,
+    null,
+  );
+
+  final sizeSmall = baseStyle.copyWith(fontSize: 10);
+  final sizeLarge = baseStyle.copyWith(fontSize: 16);
+  final sizeHuge = baseStyle.copyWith(fontSize: 18);
+
+  return DefaultStyles(
+    h1: h1,
+    h2: h2,
+    h3: h3,
+    paragraph: paragraph,
+    bold: bold,
+    subscript: subscript,
+    superscript: superscript,
+    italic: italic,
+    small: small,
+    underline: underline,
+    strikeThrough: strikeThrough,
+    inlineCode: inlineCode,
+    link: link,
+    placeHolder: placeholder,
+    lists: lists,
+    quote: quote,
+    code: code,
+    indent: indent,
+    align: align,
+    leading: leading,
+    sizeSmall: sizeSmall,
+    sizeLarge: sizeLarge,
+    sizeHuge: sizeHuge,
+  );
 }
