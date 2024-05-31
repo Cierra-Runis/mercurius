cb89728316bba90f5b5e397da7598a3880d8a4b1
Cierra_Runis <2864283875@qq.com>
2024-05-31 09:26:08 +0800

    📖Add build script

---
+++ app/script/build.py
@@ -0,0 +1,46 @@
+'''
+Build script for release
+'''
+
+import os
+from datetime import datetime
+
+builtAt = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+common = [
+    '--obfuscate', '--split-debug-info=splitMap',
+    '--dart-define-from-file=.env', f'--dart-define=builtAt="{builtAt}"',
+    '--verbose'
+]
+
+android = [
+    'flutter',
+    'build',
+    'apk',
+    '--target-platform android-arm64',
+    '--split-per-abi',
+    *common,
+]
+
+os.system(' '.join(android))
+
+androidCopy = [
+    'copy',
+    r'"build\app\outputs\flutter-apk\app-arm64-v8a-release.apk"',
+    '"out"',
+]
+
+os.system(' '.join(androidCopy))
+
+windows = ['flutter', 'build', 'windows', *common]
+
+os.system(' '.join(windows))
+
+windowsZip = [
+    'bz',
+    'c',
+    '"out/Mercurius for Windows.zip"',
+    'build/windows/x64/runner/Release',
+]
+
+os.system(' '.join(windowsZip))
